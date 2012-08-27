2defad6c13c37d87c5d027d90910529e280ce1f4
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2012-08-27 14:59:29 +0200

    Reverted some changes

--- src/main/java/eu/playproject/platform/service/bootstrap/rest/InitServiceImpl.java
+++ src/main/java/eu/playproject/platform/service/bootstrap/rest/InitServiceImpl.java
@@ -81,7 +81,8 @@ public class InitServiceImpl implements InitService {
 		String eventCloudEndpoint = null;
 		String subscriberEndpoint = null;
 		try {
-			eventCloudEndpoint = "http://138.96.19.125:8085/proactive/services/EventCloud_management-webservices";
+		    eventCloudEndpoint = endpointRegistry
+		            .get(org.ow2.play.service.registry.api.Constants.DSB_TO_EC_EC);
 			subscriberEndpoint = endpointRegistry
 					.get(org.ow2.play.service.registry.api.Constants.DSB_TO_EC_EC_SUBSCRIBER);
 		} catch (RegistryException e1) {
@@ -127,7 +128,8 @@ public class InitServiceImpl implements InitService {
 		String eventCloudEndpoint = null;
 		String dsbEndpoint = null;
 		try {
-			eventCloudEndpoint = "http://138.96.19.125:8085/proactive/services/EventCloud_management-webservices";
+			eventCloudEndpoint = endpointRegistry
+			        .get(org.ow2.play.service.registry.api.Constants.EC_TO_DSB_EC);
 			dsbEndpoint = endpointRegistry
 					.get(org.ow2.play.service.registry.api.Constants.EC_TO_DSB_DSB);
 		} catch (RegistryException e1) {
